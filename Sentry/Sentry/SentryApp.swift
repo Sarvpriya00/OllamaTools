c3e2becc02827c6493edf38bf69a054b2be63663
sarvpriya00 <sarvpriya.adarsh@gmail.com>
2026-04-15 04:55:15 +0530

    working chat interface

---
+++ Sentry/Sentry/SentryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentryApp.swift
+//  Sentry
+//
+//  Created by Sarvpriya Adarsh on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SentryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
